63f7831c36976610d1d054ec5790edd957a8b006
HALLakmina <lahirulakmina1999@gmail.com>
2023-11-08 00:45:25 +0530

    update timetable screen
    add the Timetable to timetable screen

--- Laboratory_Schedule_System_Mobile_App/Resources/Screens/IndexTimetableScreen.tsx
+++ Laboratory_Schedule_System_Mobile_App/Resources/Screens/IndexTimetableScreen.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 /* eslint-disable prettier/prettier */
-import { View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, Image} from 'react-native';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import UserLoginButton from '../Components/UserLoginButton';
@@ -15,8 +15,34 @@ const IndexHomeScreen = () => {
           <UserLoginButton/>
         </View>
       </LinearGradient>
+
+
       <View style={styles.bodyView}>
-        <Text>Timetable</Text>
+      <Image style={{width:'100%',height:'100%', position:'absolute'}}  source={ require('../../Assets/images/pexels-polina-zimmerman-3747486.jpg')}/>
+        <View style={styles.TB}>
+          <View style={styles.TR}>
+            <View style={[styles.TD, styles.timeTD]}>
+              <Text style={styles.headerText}>Time</Text>
+            </View>
+            <View style={[styles.TD, styles.headerTD]}>
+              <Text style={styles.headerText}>Monday</Text>
+            </View>
+          </View>
+
+          <View style={styles.TR}>
+            <View style={[styles.TD, styles.timeTD]}>
+              <Text style={styles.headerText}>8.30AM{'\n'}9.30AM</Text>
+            </View>
+            <View style={[styles.TD, styles.alignBox]}>
+              <View >
+                <Text>Fundamental of ICT{'\n'}1134{'\n'}P.Amritha{'\n'}THL-01</Text>
+              </View>
+              <View style={styles.actionBox}>
+                <Text>Action</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -42,6 +68,46 @@ const styles = StyleSheet.create({
     {
         backgroundColor: 'green',
         flex:1,
+    },
+    TB:
+    {
+      margin:10,
+      alignItems: 'center',
+    },
+    TD:
+    {
+      padding:10,
+      justifyContent: 'center',
+      borderWidth:2,
+      backgroundColor:'red',
+      width:250,
+      margin:2,
+    },
+    TR:
+    {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    timeTD:
+    {
+      width:80,
+    },
+    headerTD:{
+      alignItems:'center',
+    },
+    headerText:
+    {
+      fontWeight: 'bold',
+    },
+    alignBox:
+    {
+      flexDirection: 'row',
+    },
+    actionBox:
+    {
+      alignContent:'flex-end',
+      paddingHorizontal:20,
+      backgroundColor:'white',
     },
 });
 
